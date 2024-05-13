7b3883a28181b15ee5a4f283343a4be2e316e422
thepythonmomo21 <154357031+thepythonmomo21@users.noreply.github.com>
2024-05-13 12:30:36 +0300

    Update WindTurbine.java

--- WindTurbine.java
+++ WindTurbine.java
@@ -42,14 +42,8 @@ public class WindTurbine extends AbstractEnergyResource {
     public double calculateEfficiency() {
         return getEfficiency(); //
     }
-    @Override
-    public double calculateNetEnergyProduction() {
-        return calculateEnergyProduction();
-    }
-    @Override
-    public String getType() {
-        return "Wind Turbine";
-    }
+  
+   
     @Override
     public double getMaxCapacity() {
         return 0.0; 
@@ -58,15 +52,10 @@ public class WindTurbine extends AbstractEnergyResource {
     public double getCurrentCapacity() {
         return 0.0; //
     }
-    @Override
-    public String getStatus() {
-        return "Unknown";
-    }
+   
+   
     @Override
     public void setStatus(String status){
     }
-    @Override
-    public double calculatePaybackPeriod(double initialCost, double energySavingsPerYear) {
-        return 0.0;
-    }
+   
 }
